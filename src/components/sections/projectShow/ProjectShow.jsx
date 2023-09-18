50e4d2ba4938e9bcb49f121944e6cdcd4ebb059f
hoshikwyw <khaingwutyiwin1712@gmail.com>
2023-09-18 13:58:03 +0630

    pj add

--- src/components/sections/projectShow/ProjectShow.jsx
+++ src/components/sections/projectShow/ProjectShow.jsx
@@ -5,6 +5,7 @@ import pj3 from "../../../assets/projectPhoto/pj3.png";
 import pj4 from "../../../assets/projectPhoto/pj4.png";
 import pj5 from "../../../assets/projectPhoto/pj5.png";
 import pj6 from "../../../assets/projectPhoto/pj6.png";
+import pj7 from "../../../assets/projectPhoto/pj7.png";
 import "./projectShow.css";
 import Banner from "../../Banner/Banner";
 import AOS from "aos";
@@ -31,32 +32,6 @@ const ProjectShow = () => {
       <div className="w-[90%] grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 mb-24 mx-auto gap-10 ">
         {/* project card  */}
         <div
-          data-aos="fade-right"
-          data-aos-duration="1000"
-          className="cursor-pointer ">
-          <a
-            href="https://edu-pro-dashboard-c.netlify.app/"
-            className=" card dark:card-shadow duration-300 overflow-hidden py-5 flex flex-col gap-3 bg-[#F7F7FF] dark:bg-[#0B0B13] rounded-md shadow-md">
-            <img src={pj2} className="w-[100%]" />
-            <h1 className="text-[#0B0B13] dark:text-[#FFFFFF] text-lg tracking-wide font-medium text-center">
-              Dashboard Web Design
-            </h1>
-            <div className="flex my-3 gap-5 mx-auto">
-              <a
-                href="https://edu-pro-dashboard-c.netlify.app/"
-                className="go-up cursor-pointer py-2 px-5 text-white dark:text-black bg-[#0B0B13] dark:bg-white border border-[#0B0B13] dark:border-white hover:bg-transparent dark:hover:bg-transparent hover:text-[#0B0B13] dark:hover:text-white duration-[400ms] font-medium tracking-wide rounded">
-                Demo
-              </a>
-              <a
-                href="https://github.com/hoshikwyw/Edu_Pro_Dashboard"
-                className="go-up cursor-pointer py-2 px-5 text-white dark:text-black bg-[#0B0B13] dark:bg-white border border-[#0B0B13] dark:border-white hover:bg-transparent dark:hover:bg-transparent hover:text-[#0B0B13] dark:hover:text-white duration-[400ms] font-medium tracking-wide rounded">
-                Git Hub
-              </a>
-            </div>
-          </a>
-        </div>
-
-        <div
           className="cursor-pointer "
           data-aos="fade-right"
           data-aos-duration="1000"
@@ -76,6 +51,86 @@ const ProjectShow = () => {
               </a>
               <a
                 href="https://github.com/hoshikwyw/Edu-Pro"
+                className="go-up cursor-pointer py-2 px-5 text-white dark:text-black bg-[#0B0B13] dark:bg-white border border-[#0B0B13] dark:border-white hover:bg-transparent dark:hover:bg-transparent hover:text-[#0B0B13] dark:hover:text-white duration-[400ms] font-medium tracking-wide rounded">
+                Git Hub
+              </a>
+            </div>
+          </a>
+        </div>
+
+        <div
+          data-aos="fade-right"
+          data-aos-duration="1000"
+          data-aos-delay="100"
+          className="cursor-pointer">
+          <a
+            href="https://oro-music-player.netlify.app/"
+            className=" card duration-300 overflow-hidden py-5 flex flex-col gap-3 bg-[#F7F7FF] dark:bg-[#0B0B13] rounded-md shadow-md">
+            <img src={pj7} className="w-[100%]" />
+            <h1 className="text-[#0B0B13] dark:text-[#FFFFFF] text-lg tracking-wide font-medium text-center">
+              Oro's Music Player
+            </h1>
+            <div className="flex my-3 gap-5 mx-auto">
+              <a
+                href="https://oro-music-player.netlify.app/"
+                className="go-up cursor-pointer py-2 px-5 text-white dark:text-black bg-[#0B0B13] dark:bg-white border border-[#0B0B13] dark:border-white hover:bg-transparent dark:hover:bg-transparent hover:text-[#0B0B13] dark:hover:text-white duration-[400ms] font-medium tracking-wide rounded">
+                Demo
+              </a>
+              <a
+                href="https://github.com/hoshikwyw/Music-Player-App"
+                className="go-up cursor-pointer py-2 px-5 text-white dark:text-black bg-[#0B0B13] dark:bg-white border border-[#0B0B13] dark:border-white hover:bg-transparent dark:hover:bg-transparent hover:text-[#0B0B13] dark:hover:text-white duration-[400ms] font-medium tracking-wide rounded">
+                Git Hub
+              </a>
+            </div>
+          </a>
+        </div>
+
+        <div
+          data-aos="fade-right"
+          data-aos-duration="1000"
+          data-aos-delay="100"
+          className="cursor-pointer">
+          <a
+            href="https://my-food-blog-1712.netlify.app/"
+            className=" card duration-300 overflow-hidden py-5 flex flex-col gap-3 bg-[#F7F7FF] dark:bg-[#0B0B13] rounded-md shadow-md">
+            <img src={pj5} className="w-[100%]" />
+            <h1 className="text-[#0B0B13] dark:text-[#FFFFFF] text-lg tracking-wide font-medium text-center">
+              Fooddy Blog
+            </h1>
+            <div className="flex my-3 gap-5 mx-auto">
+              <a
+                href="https://my-food-blog-1712.netlify.app/"
+                className="go-up cursor-pointer py-2 px-5 text-white dark:text-black bg-[#0B0B13] dark:bg-white border border-[#0B0B13] dark:border-white hover:bg-transparent dark:hover:bg-transparent hover:text-[#0B0B13] dark:hover:text-white duration-[400ms] font-medium tracking-wide rounded">
+                Demo
+              </a>
+              <a
+                href="https://github.com/hoshikwyw/Food-Blog"
+                className="go-up cursor-pointer py-2 px-5 text-white dark:text-black bg-[#0B0B13] dark:bg-white border border-[#0B0B13] dark:border-white hover:bg-transparent dark:hover:bg-transparent hover:text-[#0B0B13] dark:hover:text-white duration-[400ms] font-medium tracking-wide rounded">
+                Git Hub
+              </a>
+            </div>
+          </a>
+        </div>
+
+        <div
+          data-aos="fade-right"
+          data-aos-duration="1000"
+          className="cursor-pointer ">
+          <a
+            href="https://edu-pro-dashboard-c.netlify.app/"
+            className=" card dark:card-shadow duration-300 overflow-hidden py-5 flex flex-col gap-3 bg-[#F7F7FF] dark:bg-[#0B0B13] rounded-md shadow-md">
+            <img src={pj2} className="w-[100%]" />
+            <h1 className="text-[#0B0B13] dark:text-[#FFFFFF] text-lg tracking-wide font-medium text-center">
+              Dashboard Web Design
+            </h1>
+            <div className="flex my-3 gap-5 mx-auto">
+              <a
+                href="https://edu-pro-dashboard-c.netlify.app/"
+                className="go-up cursor-pointer py-2 px-5 text-white dark:text-black bg-[#0B0B13] dark:bg-white border border-[#0B0B13] dark:border-white hover:bg-transparent dark:hover:bg-transparent hover:text-[#0B0B13] dark:hover:text-white duration-[400ms] font-medium tracking-wide rounded">
+                Demo
+              </a>
+              <a
+                href="https://github.com/hoshikwyw/Edu_Pro_Dashboard"
                 className="go-up cursor-pointer py-2 px-5 text-white dark:text-black bg-[#0B0B13] dark:bg-white border border-[#0B0B13] dark:border-white hover:bg-transparent dark:hover:bg-transparent hover:text-[#0B0B13] dark:hover:text-white duration-[400ms] font-medium tracking-wide rounded">
                 Git Hub
               </a>
@@ -143,38 +198,11 @@ const ProjectShow = () => {
           data-aos-delay="100"
           className="cursor-pointer">
           <a
-            href="https://my-food-blog-1712.netlify.app/"
-            className=" card duration-300 overflow-hidden py-5 flex flex-col gap-3 bg-[#F7F7FF] dark:bg-[#0B0B13] rounded-md shadow-md">
-            <img src={pj5} className="w-[100%]" />
-            <h1 className="text-[#0B0B13] dark:text-[#FFFFFF] text-lg tracking-wide font-medium text-center">
-              Fooddy Blog
-            </h1>
-            <div className="flex my-3 gap-5 mx-auto">
-              <a
-                href="https://my-food-blog-1712.netlify.app/"
-                className="go-up cursor-pointer py-2 px-5 text-white dark:text-black bg-[#0B0B13] dark:bg-white border border-[#0B0B13] dark:border-white hover:bg-transparent dark:hover:bg-transparent hover:text-[#0B0B13] dark:hover:text-white duration-[400ms] font-medium tracking-wide rounded">
-                Demo
-              </a>
-              <a
-                href="https://github.com/hoshikwyw/Food-Blog"
-                className="go-up cursor-pointer py-2 px-5 text-white dark:text-black bg-[#0B0B13] dark:bg-white border border-[#0B0B13] dark:border-white hover:bg-transparent dark:hover:bg-transparent hover:text-[#0B0B13] dark:hover:text-white duration-[400ms] font-medium tracking-wide rounded">
-                Git Hub
-              </a>
-            </div>
-          </a>
-        </div>
-
-        <div
-          data-aos="fade-right"
-          data-aos-duration="1000"
-          data-aos-delay="100"
-          className="cursor-pointer">
-          <a
             href="https://meterbill-calculating-6def80.netlify.app/"
             className=" card duration-300 overflow-hidden py-5 flex flex-col gap-3 bg-[#F7F7FF] dark:bg-[#0B0B13] rounded-md shadow-md">
             <img src={pj6} className="w-[100%]" />
             <h1 className="text-[#0B0B13] dark:text-[#FFFFFF] text-lg tracking-wide font-medium text-center">
-            Meter-bill calculator
+              Meter-bill calculator
             </h1>
             <div className="flex my-3 gap-5 mx-auto">
               <a
